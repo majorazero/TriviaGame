b69be026e8e64ce14d5a074ff214634c0abefe22
majorazero <daniel.cw.hsu@gmail.com>
2018-08-28 09:59:06 -0700

    added image assets and installed a timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,6 +5,9 @@ let currQues;
 let quesChoice;
 //insert the correct Answer somewhere (random) and saves the correct answer index
 let corrAnsIndex;
+let ansCorrect;
+let ansIncorrect;
+let noAns;
 init();
 randomQuestionOutput();
 roundSet();
@@ -12,27 +15,43 @@ roundSet();
 function init(){
   questionSets = [
     {Ques: "What Pokemon League has Ash Ketchum, the one who's gonna be the very best, won?",
-      CorrAns: "None, because he's a the very worst.",
-      Other: ["Orange League.", "Sinoh League.", "Johto League."]
+      CorrAns: "None, because he's the very worst.",
+      Other: ["Orange League.", "Sinoh League.", "Johto League."],
+      picSrc: "assets/images/ques1.jpg"
     },
     {Ques: "Which of these concepts has NOT been made into a Pokemon?",
       CorrAns: "A cheese wheel.",
-      Other: ["Ice cream.", "Piles of garbage.", "A regular dog."]
+      Other: ["Ice cream.", "Piles of garbage.", "A regular dog."],
+      picSrc: "assets/images/ques2.png"
     },
     {Ques: "Pokemon has aired since 1997; Ash has traveled across the world and had countless adventures since he was 10 years old. How old is he now?",
       CorrAns: "10.",
-      Other: ["31.", "42.", "Eleventy-Six."]
+      Other: ["31.", "42.", "Eleventy-Six."],
+      picSrc: "assets/images/ques3.jpg"
     },
     {Ques: "Which of these Pokemon promotes childhood obesity?",
       CorrAns: "Snorlax.",
-      Other: ["Pikachu.", "Cosmog.", "Klink."]
+      Other: ["Pikachu.", "Cosmog.", "Klink."],
+      picSrc: "assets/images/ques4.jpg"
     }
   ];
 }
 
 function roundSet(){
+  let roundTime = setTimeout(function(){
+      timeUp();
+    },30000);
   //set up timer display
-  $("#timer").html("<h2>Time Remaining: 8 Seconds</h2>");
+  let countdownTime = 30;
+  let countDown = setInterval(function(){
+    if (countdownTime > 0){
+      countdownTime--;
+      $("#timer").html("<h2>Time Remaining: "+countdownTime+" Seconds</h2>");
+    }
+    else{
+      $("#timer").html("");
+    }
+  },1000);
   //lets set up the question display, first set the question
   $("#question").html("<h2>"+currQues.Ques+"</h2");
   //we'll now set up the questions.
@@ -45,6 +64,7 @@ function roundSet(){
     if(i === corrAnsIndex){
       ansButton.on("click",function (){
         console.log("You got it!");
+        win();
       });
     }
     else {
@@ -58,10 +78,20 @@ function roundSet(){
   console.log(currQues.Ques,quesChoice);
 }
 
+function win(){
+  $("#answers").html("");
+  $("#question").html("<h2>Awwww YEEE!</h2>");
+  $("#picture").html("<img src="+currQues.picSrc+" />");
+}
 function loss(){
-  console.log(currQues);
   $("#question").html("<h2>NOPE!</h2>");
   $("#answers").html("<h2>The correct answer was: "+currQues.CorrAns+"</h2>");
+  $("#picture").html("<img src="+currQues.picSrc+" />");
+}
+function timeUp(){
+  $("#answers").html("");
+  $("#question").html("<h2>Times Up!</h2>");
+  $("#picture").html("<img src="+currQues.picSrc+" />");
 }
 
 function randomQuestionOutput(){
