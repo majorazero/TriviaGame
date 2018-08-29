06334839f6f0297f12f9e967432741d75b247d91
majorazero <daniel.cw.hsu@gmail.com>
2018-08-28 19:26:51 -0700

    added end screen and point increment

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,42 +8,54 @@ let corrAnsIndex;
 let ansCorrect;
 let ansIncorrect;
 let noAns;
+//interval storer
+let countDown;
+//timeoutStorer
+let roundTime;
 init();
 randomQuestionOutput();
-roundSet();
 
 function init(){
   questionSets = [
     {Ques: "What Pokemon League has Ash Ketchum, the one who's gonna be the very best, won?",
       CorrAns: "None, because he's the very worst.",
       Other: ["Orange League.", "Sinoh League.", "Johto League."],
-      picSrc: "assets/images/ques1.jpg"
+      picSrc: "assets/images/ques1.jpg",
+      comment: "Seriously, what an idiot."
     },
     {Ques: "Which of these concepts has NOT been made into a Pokemon?",
       CorrAns: "A cheese wheel.",
       Other: ["Ice cream.", "Piles of garbage.", "A regular dog."],
-      picSrc: "assets/images/ques2.png"
+      picSrc: "assets/images/ques2.png",
+      comment: "Seriously though, there is a pokemon that is literally just a Yorkshire Terrier."
     },
     {Ques: "Pokemon has aired since 1997; Ash has traveled across the world and had countless adventures since he was 10 years old. How old is he now?",
       CorrAns: "10.",
       Other: ["31.", "42.", "Eleventy-Six."],
-      picSrc: "assets/images/ques3.jpg"
+      picSrc: "assets/images/ques3.jpg",
+      comment: "I think Misty is 35 now."
     },
     {Ques: "Which of these Pokemon promotes childhood obesity?",
       CorrAns: "Snorlax.",
       Other: ["Pikachu.", "Cosmog.", "Klink."],
-      picSrc: "assets/images/ques4.jpg"
+      picSrc: "assets/images/ques4.jpg",
+      comment: "Diabetes incarnate."
     }
   ];
+  ansCorrect = 0;
+  ansIncorrect = 0;
+  noAns = 0;
 }
 
 function roundSet(){
-  let roundTime = setTimeout(function(){
+  roundTime = setTimeout(function(){
       timeUp();
     },30000);
   //set up timer display
   let countdownTime = 30;
-  let countDown = setInterval(function(){
+  //need this here so it starts at 30 seconds
+  $("#timer").html("<h2>Time Remaining: "+countdownTime+" Seconds</h2>");
+  countDown = setInterval(function(){
     if (countdownTime > 0){
       countdownTime--;
       $("#timer").html("<h2>Time Remaining: "+countdownTime+" Seconds</h2>");
@@ -56,7 +68,7 @@ function roundSet(){
   $("#question").html("<h2>"+currQues.Ques+"</h2");
   //we'll now set up the questions.
   for(let i = 0; i < quesChoice.length; i++){
-    var ansButton = $("<button>");
+    let ansButton = $("<button>");
     ansButton.addClass("ansButton");
     //button should display the choice.
     ansButton.html("<h2>"+quesChoice[i]+"<h2>");
@@ -79,22 +91,50 @@ function roundSet(){
 }
 
 function win(){
+  //increment correct answer
+  ansCorrect++;
+  nextQuestion();
   $("#answers").html("");
   $("#question").html("<h2>Awwww YEEE!</h2>");
   $("#picture").html("<img src="+currQues.picSrc+" />");
+  $("#gameMessage").html("<h2>"+currQues.comment+"</h2>");
 }
 function loss(){
+  //increment wrong answer
+  ansIncorrect++;
+  nextQuestion();
   $("#question").html("<h2>NOPE!</h2>");
   $("#answers").html("<h2>The correct answer was: "+currQues.CorrAns+"</h2>");
   $("#picture").html("<img src="+currQues.picSrc+" />");
 }
 function timeUp(){
-  $("#answers").html("");
+  //increment no answers
+  noAns++;
+  nextQuestion();
+  $("#answers").html("<h2>The correct answer was: "+currQues.CorrAns+"</h2>");
   $("#question").html("<h2>Times Up!</h2>");
   $("#picture").html("<img src="+currQues.picSrc+" />");
+  $("#gameMessage").html("<h2>"+currQues.comment+"</h2>");
+}
+
+//will start a timeout that plays the next question after 5 seconds
+function nextQuestion(){
+  //we'll start by clearing all the previous timers
+  clearInterval(countDown);
+  clearTimeout(roundTime);
+  $("#timer").empty();
+  //I don't think this will bite me in the ass but we'll see.
+  countdown = setTimeout(function(){
+    //empty out prev picture and comments
+    $("#picture").html("");
+    $("#gameMessage").html("");
+    //outputs a new question and resets round
+    randomQuestionOutput();
+  },5000);
 }
 
 function randomQuestionOutput(){
+  //if there are still questions left
   if(questionSets.length != 0){
     currQues = questionSets.splice(Math.floor(Math.random()*questionSets.length),1)[0];
     //we'll set an array of the possible OTHER answers
@@ -102,5 +142,15 @@ function randomQuestionOutput(){
     //insert the correct Answer somewhere (random) and saves the correct answer index
     corrAnsIndex = Math.floor(Math.random()*quesChoice.length);
     quesChoice.splice(corrAnsIndex,0,currQues.CorrAns);
+    //we'll set the next round
+    roundSet();
+  }
+  else {
+    clearInterval(countDown);
+    //end screen
+    $("#question").html("<h2>Game Over!<h2>");
+    $("#answers").html("<h2>Correct Answer: "+ansCorrect+
+                        "<br>Incorrect Answer: "+ansIncorrect+
+                        "<br>Unanswered: "+noAns+"</h2>");
   }
 }
